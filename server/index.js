bb5754bbd66b935b0d853f5431a87234c0e6d3a8
stephenJohnGiardina <stephengia@yahoo.com>
2020-07-22 17:31:42 -0400

    Added comments to server file

--- server/index.js
+++ server/index.js
@@ -12,6 +12,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/../public')));
 
 app.get('/login', (req, res) => {
+  /**
+   * This is a route for logging in. If the username exists the data associated
+   * with that user will be sent back to the client. If the username does not
+   * exist a 404 error will be sent back to the client.
+   */
   const { user } = req.query;
   db.login(user, (err, doc) => {
     if (err) {
@@ -24,6 +29,11 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/newUser', (req, res) => {
+  /**
+   * This is a route for creating a new account. It takes in a new username. If
+   * the username does not exist, a new account is made with the username given and
+   * the client will be notified. If the username exists the client will also be notified.
+   */
   const { newUsername } = req.body;
   db.createUser(newUsername, (err, doc) => {
     if (err) {
@@ -36,6 +46,11 @@ app.post('/newUser', (req, res) => {
 });
 
 app.post('/newChore', (req, res) => {
+  /**
+   * This is a route for creating a new chore. It takes in the data for a chore. The
+   * user the client is currently logged in as is looked up and the chore data gets
+   * added to the user's account.
+   */
   const { chores, choreData, user } = JSON.parse(req.body.post);
   chores.push(choreData);
   db.createChore(user, chores, (err, userData) => {
@@ -49,6 +64,10 @@ app.post('/newChore', (req, res) => {
 });
 
 app.delete('/deleteAccount', (req, res) => {
+  /**
+   * This is a route for deleting an account. If the account entered exists the account
+   * will be removed from the database. If not nothing happens.
+   */
   const { deleteAccountName } = req.body;
   db.deleteAccount(deleteAccountName, (err, result) => {
     if (err) {
@@ -73,6 +92,13 @@ app.delete('/deleteChore', (req, res) => {
 });
 
 app.put('/editChore', (req, res) => {
+  /**
+   * This is a route for editing a chore. It takes in an object called editData which
+   * holds the username and the new data for the chore to be edited (choreData). The
+   * username is used to query the account. When the account is found the chore that
+   * is going to be edited is looked up using an index that is found on the choreData
+   * object. Once that chore is found its data is replaced with the new data.
+   */
   const editData = JSON.parse(req.body.editData);
   db.editChore(editData, (err, doc, index) => {
     if (err) {
